0ca19a55741f069556ac6638bb7dc6d67b64e011
RogerCruz14 <roger.cruzbsc@gmail.com>
2023-05-17 23:51:38 -0700

    first commit

---
+++ public/js/cart.js
@@ -0,0 +1,90 @@
+function setCarritoVacio() {
+  cartRows.innerHTML = `
+    <tr>     
+        <td colspan="5"><div class="alert alert-warning my-2 text-center">No tienes products en el carrito</div></td>
+    </tr>            
+    `;
+}
+function vaciarCarrito() {
+  localStorage.removeItem("carrito");
+}
+
+function calcularTotal(products) {
+  return products.reduce(
+    (acum, product) => (acum += product.price * product.quantity),
+    0
+  );
+}
+
+let cartRows = document.querySelector('.cartRows');
+
+let products = [];
+
+if(localStorage.carrito){
+  let carrito = JSON.parse(localStorage.carrito);
+  // console.log(carrito);
+  carrito.forEach((item, index) => {
+    fetch(`/api/product/${item.id}`)
+      .then(res=>res.json())
+      .then(product => {
+        if(product){
+          cartRows.innerHTML += `
+        <tr id="row${index}">
+            <th scope = "row">${index + 1}</th>
+            <td>${product.name}</td>
+            <td>$ ${product.price}</td>
+            <td class="text-center">${item.quantity}</td>
+            <td class="text-center">$ ${parseFloat(
+              product.price * item.quantity,
+              2
+            ).toFixed(2)}</td>
+            <td><button class="btn btn-danger btn-sm" onclick=removeItem(${index})><i class="fas fa-trash">
+        `;
+        products.push({
+          producId: product.id,
+          name: product.name,
+          price: product.price,
+          quantity: item.quantity,
+        });
+        } else{
+          /* Lo borro de lcoal storage */
+          carrito.splice(index,1)
+          localStorage.setItem('carrito', JSON.stringify(carrito));
+        }
+    }).then(() =>{
+      document.querySelector(".totalAmount").innerText = 
+      `$ ${calcularTotal(
+        products
+        )}`;
+    });
+  });
+}
+
+
+
+let checkoutCart = document.querySelector('#checkoutCart');
+
+checkoutCart.onsubmit = (e) =>{
+  e.preventDefault();
+  const formData = {
+    orderItems: products,
+    paymentMethod: checkoutCart.paymentMethod.value,
+    shippingMethod: checkoutCart.shippingMethod.value,
+    total: calcularTotal(products),
+  };
+  fetch("/api/checkout",{
+    method:"POST",
+    headers:{
+      "Content-Type":"application/json"
+    },
+    body: JSON.stringify(formData)
+  })
+  .then((r) => r.json())
+  .then((res) => {
+    if(res.ok){
+      vaciarCarrito()
+      location.href = `/order/${res.order.id}`;
+    }
+  }); 
+  console.log(formData);
+};
---
+++ src/routes/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+
+const controller = require("../controllers/homeController");
+const authMiddleware = require("../middlewares/authMiddleware");
+/* GET home page. */
+router.get("/", controller.home);
+//router.get("/cart", controller.cart);
+router.get("/cart", authMiddleware, controller.cart);
+router.get("/order/:id", authMiddleware, controller.order);
+
+module.exports = router;
